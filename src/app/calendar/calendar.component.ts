6568ac3e8ee897eb261842179411ff39b7bdc568
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-02-21 14:27:00 -0300

    [calendar] Moved calendar build logic from template to componnent. Trailing months

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -9,21 +9,50 @@ import * as moment from 'moment';
 export class CalendarComponent implements OnInit {
   currentSelection: string;
   weekDays: Array<string> = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  calendar: Array<Object> = [];
   today = moment();
 
-  calendar: Object = {
+  currentMonth: Object = {
     day: this.today.clone().date(),
-    month: this.today.clone().month(),
-    year: this.today.clone().year(),
-    date: this.today.clone().format('YYYY-MM'),
-    daysInMonth: Array(this.today.clone().daysInMonth())
+    month: this.today.clone().format('MM'),
+    year: this.today.clone().format('YYYY'),
+    daysInMonth: this.today.clone().daysInMonth()
   }
 
+  lastMonth: Object = {
+    day: this.today.clone().startOf('month').startOf('week').date(),
+    month: this.today.clone().startOf('month').startOf('week').format('MM'),
+    year: this.today.clone().startOf('month').startOf('week').format('YYYY'),
+    days: this.today.clone().startOf('month').startOf('week').daysInMonth()
+  }
+
+  nextMonth: Object = {
+    day: this.today.clone().endOf('month').endOf('week').date(),
+    month: this.today.clone().endOf('month').endOf('week').format('MM'),
+    year: this.today.clone().endOf('month').endOf('week').format('YYYY')
+  }
+
+  buildCalendar(): void {
+    let [m1, m2, m3] = [this.lastMonth, this.currentMonth, this.nextMonth];
+
+    this.buildMonth(m1['day'], m1['days'], m1['month'], m1['year'])
+    this.buildMonth(1, m2['daysInMonth'], m2['month'], m2['year'])
+    this.buildMonth(1, m3['day'], m3['month'], m3['year'])
+  }
+
+  buildMonth(start: number, end: number, month: string, year: string): void {
+    for (let i = start; i <= end; i++) {
+      let day = i < 10 ? '0' + i : i.toString(),
+          date = `${year}-${month}-${day}`
+
+      this.calendar.push({ day: day, month: month, year: year, date: date });
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
-
+    this.buildCalendar();
   }
 
   isSelected(day: string): boolean {
